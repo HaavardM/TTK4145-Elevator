dbca6a16853b87120578a0bcf11476dae44a139a
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-02-14 17:12:31 +0100

    Add elevator module and example

---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/TTK4145-students-2019/project-thefuturezebras/internal/elevatordriver"
+	"github.com/TTK4145/driver-go/elevio"
+)
+
+func main() {
+	arrivedAtFloor := make(chan int)
+	elevatorCommand := make(chan elevatordriver.Command)
+	elevatorEvents := make(chan elevatordriver.Event)
+	onButtonPress := make(chan elevio.ButtonEvent)
+	lightState := make(chan elevatordriver.LightState)
+	conf := elevatordriver.Config{
+		Address:        "localhost:8080",
+		NumberOfFloors: 4,
+		ArrivedAtFloor: arrivedAtFloor,
+		Commands:       elevatorCommand,
+		Events:         elevatorEvents,
+		OnButtonPress:  onButtonPress,
+		SetStatusLight: lightState,
+	}
+	ctx := context.Background()
+	go elevatordriver.Run(ctx, conf)
+	fmt.Println("Hello!")
+	dir := elevatordriver.MoveUp
+	for {
+		select {
+		case f := <-arrivedAtFloor:
+			lightState <- elevatordriver.LightState{Type: elevatordriver.AllLights, State: false, Floor: f}
+			elevatorCommand <- elevatordriver.Stop
+			if f == conf.NumberOfFloors-1 {
+				dir = elevatordriver.MoveDown
+			} else if f == 0 {
+				dir = elevatordriver.MoveUp
+			}
+		case <-time.After(5 * time.Second):
+			elevatorCommand <- dir
+		case b := <-onButtonPress:
+			switch b.Button {
+			case elevio.BT_HallDown:
+				lightState <- elevatordriver.LightState{Type: elevatordriver.DownButtonLight, State: true, Floor: b.Floor}
+			case elevio.BT_HallUp:
+				lightState <- elevatordriver.LightState{Type: elevatordriver.UpButtonLight, State: true, Floor: b.Floor}
+			case elevio.BT_Cab:
+				lightState <- elevatordriver.LightState{Type: elevatordriver.InternalButtonLight, State: true, Floor: b.Floor}
+			}
+		}
+	}
+
+}
--- internal/elevatordriver/elevatordriver.go
+++ internal/elevatordriver/elevatordriver.go
@@ -1,35 +1,142 @@
 package elevatordriver
 
-import "fmt"
+import (
+	"context"
+	"errors"
 
-type CommandID int
-type Event int
-
-const (
-	CloseDoor           CommandID = 1
-	OpenDoor            CommandID = 2
-	MoveUp              CommandID = 3
-	MoveDown            CommandID = 4
-	Stop                CommandID = 5
-	SetOrderLightUp     CommandID = 6
-	ClearOrderLightUp   CommandID = 7
-	SetOrderLightDown   CommandID = 8
-	ClearOrderLightDown CommandID = 9
-	SetInternalLight    CommandID = 10
-	ClearInternalLight  CommandID = 11
+	"github.com/TTK4145/driver-go/elevio"
 )
 
-type Command struct {
-	command CommandID
-	floor   int
+//Command used to send elevator commands
+type Command int
+
+//Event used to send events from elevator
+type Event int
+
+//LightType used to differentiate between types of lights
+type LightType int
+
+//LightState is used to request a light state
+type LightState struct {
+	Type  LightType
+	State bool
+	Floor int
 }
 
+const (
+	/******************Commands***************************/
+
+	//CloseDoor closes elevator door
+	CloseDoor Command = iota + 1
+	//OpenDoor opens elevator door
+	OpenDoor
+	//MoveUp starts motor with direction up
+	MoveUp
+	//MoveDown starts motor with direction down
+	MoveDown
+	//Stop stops the motor
+	Stop
+	/*****************************Events*****************************/
+
+	//StopPressed indicates the stop button have been pressed
+	StopPressed Event = iota + 1
+	//StopReleased indicates the stop button have been released
+	StopReleased
+
+	/****************************Button Types************************/
+
+	//UpButtonLight is the floor order buttons upwards
+	UpButtonLight LightType = iota + 1
+	//DownButtonLight is the floor order buttons downwards
+	DownButtonLight
+	//InternalButtonLight is the internal order buttons
+	InternalButtonLight
+	//AllLights used to set all lights at floor
+	AllLights
+)
+
+//Config contains neccessary configuration for the elevator driver
 type Config struct {
-	commands       <-chan Command
-	events         chan<- Event
-	arrivedAtFloor chan<- int
+	Address        string
+	NumberOfFloors int
+	Commands       <-chan Command
+	SetStatusLight <-chan LightState
+	Events         chan<- Event
+	ArrivedAtFloor chan<- int
+	OnButtonPress  chan<- elevio.ButtonEvent
 }
 
-func Run(config Config) {
-	fmt.Println("Hello")
+//Run runs the elevator driver module
+func Run(ctx context.Context, config Config) {
+	stopSignal := make(chan bool)
+	arrivedAtFloor := make(chan int)
+	//Initialize elevio module
+	elevio.Init(config.Address, config.NumberOfFloors)
+	//Start button poller
+	go elevio.PollButtons(config.OnButtonPress)
+	//Start floor sensor poller
+	go elevio.PollFloorSensor(arrivedAtFloor)
+	//Start stop button poller
+	go elevio.PollStopButton(stopSignal)
+
+	//Run infite loop until context finishes
+	for {
+		select {
+		case c := <-config.Commands:
+			handleNewCommand(c)
+		case l := <-config.SetStatusLight:
+			handleNewLightState(l)
+		case s := <-stopSignal:
+			config.Events <- getStopEvent(s)
+		case f := <-arrivedAtFloor:
+			elevio.SetFloorIndicator(f)
+			config.ArrivedAtFloor <- f
+		case <-ctx.Done():
+			break
+		default:
+		}
+	}
+}
+
+func getStopEvent(s bool) Event {
+	if s {
+		return StopPressed
+	}
+	return StopReleased
+}
+
+func handleNewCommand(cmd Command) error {
+	switch cmd {
+	case CloseDoor:
+		elevio.SetDoorOpenLamp(false)
+	case OpenDoor:
+		elevio.SetDoorOpenLamp(true)
+	case MoveUp:
+		elevio.SetMotorDirection(elevio.MD_Up)
+	case MoveDown:
+		elevio.SetMotorDirection(elevio.MD_Down)
+	case Stop:
+		elevio.SetMotorDirection(elevio.MD_Stop)
+	default:
+		return errors.New("ElevatorDriver: Command not recognized")
+	}
+	return nil
+}
+
+func handleNewLightState(light LightState) error {
+	switch light.Type {
+	case UpButtonLight:
+		elevio.SetButtonLamp(elevio.BT_HallUp, light.Floor, light.State)
+	case DownButtonLight:
+		elevio.SetButtonLamp(elevio.BT_HallDown, light.Floor, light.State)
+	case InternalButtonLight:
+		elevio.SetButtonLamp(elevio.BT_Cab, light.Floor, light.State)
+	case AllLights:
+		elevio.SetButtonLamp(elevio.BT_HallUp, light.Floor, light.State)
+		elevio.SetButtonLamp(elevio.BT_HallDown, light.Floor, light.State)
+		elevio.SetButtonLamp(elevio.BT_Cab, light.Floor, light.State)
+	default:
+		return errors.New("Unrecognized light type")
+	}
+	return nil
 }
