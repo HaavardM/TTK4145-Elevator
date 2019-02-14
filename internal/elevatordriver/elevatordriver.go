c8bcd795f67984324d9d2bea88b216c59c814f0f
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-02-14 13:51:27 +0100

    Add elevator driver interface

--- internal/elevatordriver/elevatordriver.go
+++ internal/elevatordriver/elevatordriver.go
@@ -2,6 +2,34 @@ package elevatordriver
 
 import "fmt"
 
-func main() {
-	fmt.Println("vim-go")
+type CommandID int
+type Event int
+
+const (
+	CloseDoor           CommandID = 1
+	OpenDoor            CommandID = 2
+	MoveUp              CommandID = 3
+	MoveDown            CommandID = 4
+	Stop                CommandID = 5
+	SetOrderLightUp     CommandID = 6
+	ClearOrderLightUp   CommandID = 7
+	SetOrderLightDown   CommandID = 8
+	ClearOrderLightDown CommandID = 9
+	SetInternalLight    CommandID = 10
+	ClearInternalLight  CommandID = 11
+)
+
+type Command struct {
+	command CommandID
+	floor   int
+}
+
+type Config struct {
+	commands       <-chan Command
+	events         chan<- Event
+	arrivedAtFloor chan<- int
+}
+
+func Run(config Config) {
+	fmt.Println("Hello")
 }
