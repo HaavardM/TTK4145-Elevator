6ea62e3548632fe223b05d2f6c122050ea7fd8b7
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-03-15 15:55:45 +0100

    Add configuration params to config

--- main.go
+++ main.go
@@ -2,38 +2,55 @@ package main
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/TTK4145-students-2019/project-thefuturezebras/internal/configuration"
 	"github.com/TTK4145-students-2019/project-thefuturezebras/internal/elevatorcontroller"
 	"github.com/TTK4145-students-2019/project-thefuturezebras/internal/elevatordriver"
 	"github.com/TTK4145/driver-go/elevio"
 )
 
 func main() {
+	//Main context
+	ctx := context.Background()
+
+	//Get configration
+	conf := configuration.GetConfig()
+
+	//Create neccessary channels for the elevator
 	arrivedAtFloor := make(chan int)
 	elevatorCommand := make(chan elevatordriver.Command)
 	elevatorEvents := make(chan elevatordriver.Event)
 	onButtonPress := make(chan elevio.ButtonEvent)
 	lightState := make(chan elevatordriver.LightState)
+	orders := make(chan []elevatorcontroller.Order)
+
+	//Create elevator configuration
 	elevatorConf := elevatordriver.Config{
-		Address:        "localhost:15657",
-		NumberOfFloors: 4,
+		Address:        fmt.Sprintf("localhost:%d", conf.ElevatorPort),
+		NumberOfFloors: conf.Floors,
 		ArrivedAtFloor: arrivedAtFloor,
 		Commands:       elevatorCommand,
 		Events:         elevatorEvents,
 		OnButtonPress:  onButtonPress,
 		SetStatusLight: lightState,
 	}
-	orders := make(chan []elevatorcontroller.Order)
+
+	//Create elevator controller configuration
 	controllerConf := elevatorcontroller.Config{
 		ElevatorCommand: elevatorCommand,
 		ElevatorEvents:  elevatorEvents,
 		Orders:          orders,
 		ArrivedAtFloor:  arrivedAtFloor,
 	}
-	ctx := context.Background()
+
+	//Launch modules
 	go elevatordriver.Run(ctx, elevatorConf)
+	go elevatorcontroller.Run(ctx, controllerConf)
 	go newButtonPressed(ctx, onButtonPress, orders)
-	elevatorcontroller.Run(ctx, controllerConf)
+
+	//Wait for completion
+	<-ctx.Done()
 }
 
 func newButtonPressed(ctx context.Context, onButtonPress <-chan elevio.ButtonEvent, elevatorOrders chan<- []elevatorcontroller.Order) {
