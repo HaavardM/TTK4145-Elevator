4b15ba22b07862bea12fbba13e72bb2a947abfb4
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-03-18 13:45:26 +0100

    Add test code

--- scripts/start_elevator.bash
+++ scripts/start_elevator.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "Enter ID"
+read id
+
 start_elevator () {
     sleep 2
-    docker run --network host thefuturezebras/project:$(git rev-parse HEAD)
+    docker run --network host thefuturezebras/project:$(git rev-parse HEAD) ./main --id=$id
 }
 
 start_elevator&
