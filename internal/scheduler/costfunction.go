92e7902b20f2af764cdb9470b04ab2991b958cc7
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-03-26 15:11:03 +0100

    Fix cost stuff

--- internal/scheduler/costfunction.go
+++ internal/scheduler/costfunction.go
@@ -1,56 +1,55 @@
-package scehduler
+package scheduler
 
 import (
-		"github.com/TTK4145-students-2019/project-thefuturezebras/internal/common"
+	"github.com/TTK4145-students-2019/project-thefuturezebras/internal/common"
 )
 
 func updateElevatorCost(conf Config, costs *common.OrderCosts, status common.ElevatorStatus) {
-	penaltyToTarget := 7
-	penaltyTargetDir := 3
-	cabPenalty := 4
+	penaltyToTarget := 7.0
+	penaltyTargetDir := 3.0
+	cabPenalty := 4.0
 
 	for i := 0; i < conf.NumFloors; i++ {
-		cost := (i - ElevatorFloot)*2	
+		cost := float64(i-status.Floor) * 2.0
 
-
-		if cost < 0	{							
-			if status.Dir == DownDir	||	status.Dir == NoDir {
-				cost = -cost 
-				costs.CostsUp[i] = penaltyTargetDir*cost
+		if cost < 0 {
+			if status.Dir == common.DownDir || status.Dir == common.NoDir {
+				cost = -cost
+				costs.CostsUp[i] = penaltyTargetDir * cost
 				costs.CostsDown[i] = cost
-				costs.CostsCab[i] = cabPenalty*cost 
+				costs.CostsCab[i] = cabPenalty * cost
 			}
-		}
-		else if cost > 0 {									
-			if status.Dir == UpDir	||	status.Dir == NoDir {
+		} else if cost > 0 {
+			if status.Dir == common.UpDir || status.Dir == common.NoDir {
 				costs.CostsUp[i] = cost
-				costs.CostsDown[i] = penaltyTargetDir*cost
-				costs.CostsCab[i] = cabPenalty*cost
+				costs.CostsDown[i] = penaltyTargetDir * cost
+				costs.CostsCab[i] = cabPenalty * cost
 			}
-		}
-		else if cost == 0	{
+		} else if cost == 0 {
 			cost = 1
-			if status.Dir == NoDir {						
+			if status.Dir == common.NoDir {
 				costs.CostsUp[i] = cost
 				costs.CostsDown[i] = cost
 				costs.CostsCab[i] = cost
 			}
 		}
-		if (cost < 0 && status.Dir == UpDir)	||	(cost == 0 && status.Dir == UpDir) {
+		if (cost < 0 && status.Dir == common.UpDir) || (cost == 0 && status.Dir == common.UpDir) {
 			if cost < 0 {
 				cost = -cost
-			} else {cost = 1}
-			costs.CostsUp[i] = penaltyToTarget*penaltyTargetDir*cost
-			costs.CostsDown[i] = penaltyToTarget*cost
-			costs.CostsCab[i] = penaltyToTarget*cabPenalty*cost 		
+			} else {
+				cost = 1
+			}
+			costs.CostsUp[i] = penaltyToTarget * penaltyTargetDir * cost
+			costs.CostsDown[i] = penaltyToTarget * cost
+			costs.CostsCab[i] = penaltyToTarget * cabPenalty * cost
 		}
-		if  (cost > 0 && status.Dir == DownDir)	||	(cost == 0 && status.Dir == DownDir) {
+		if (cost > 0 && status.Dir == common.DownDir) || (cost == 0 && status.Dir == common.DownDir) {
 			if cost == 0 {
 				cost = 1
 			}
-			costs.CostsUp[i] = penaltyToTarget*cost
-			costs.CostsDown[i] = penaltyToTarget*penaltyTargetDir*cost
-			costs.CostsCab[i] = penaltyToTarget*cabPenalty*cost
-		}		
+			costs.CostsUp[i] = penaltyToTarget * cost
+			costs.CostsDown[i] = penaltyToTarget * penaltyTargetDir * cost
+			costs.CostsCab[i] = penaltyToTarget * cabPenalty * cost
+		}
 	}
 }
