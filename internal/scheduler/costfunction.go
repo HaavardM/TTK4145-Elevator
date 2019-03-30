d7221fe0f40b706c75b2041dfeb6c52a63a9fd8b
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-03-30 14:56:05 +0100

    Rename costfile

--- internal/scheduler/costfunction.go
+++ internal/scheduler/costfunction.go
@@ -1,71 +1,128 @@
 package scheduler
 
-/*
 import (
+	"math"
+
 	"github.com/TTK4145-students-2019/project-thefuturezebras/internal/common"
 )
 
+func createElevatorCost(status common.ElevatorStatus, orders *schedOrders, id int) common.OrderCosts {
 
-//Updates the cost of an elevator when the status of the elevator changes.
-//Assigning an elevator to an order is based on the cost of the elevator to perform the specific order.
-func updateElevatorCost(conf Config, costs *common.OrderCosts, status common.ElevatorStatus) {
-	//Penalty if the elevator has to change direction to get to the floor of the order
-	penaltyToTarget := 7.0
-	//Penalty if the elevator gets to a floor and then has to change direction to perform the order
-	penaltyTargetDir := 3.0
-	//Penalty for cabc alls so that direction specific orders are prioritized over cabcalls at the same floor.
-	//calls up and down also clear cab calls
-	cabPenalty := 4.0
+	//Count orders
+	orderCount := countOrdersWithID(orders, id)
 
-	for i := 0; i < conf.NumFloors; i++ {
-		//gerneral cost is the distance between the last floor of the elevator and the floor of the order
-		cost := float64(i-status.Floor) * 2.0
+	//Add a small penalty to cab call so that hall orders are prioritized at the same floor
+	//Hall orders can clear cab orders, but not the other way around.
+	cabPenalty := 0.5
+	//Create new cost table
+	newCost := common.OrderCosts{
+		ID:         id,
+		OrderCount: orderCount,
+		Cab:        make([]float64, len(orders.Cab)),      //All zeros
+		HallUp:     make([]float64, len(orders.HallUp)),   //All zeros
+		HallDown:   make([]float64, len(orders.HallDown)), //All zeros
+	}
 
-		//Negative cost means the order is below last floor of the elevator
-		if cost < 0 {
-			if status.Dir == common.DownDir || status.Dir == common.NoDir {
-				cost = -cost
-				costs.CostsUp[i] = penaltyTargetDir * cost
-				costs.CostsDown[i] = cost
-				costs.CostsCab[i] = cabPenalty * cost
-			}
-		//Positive cost means the order is above last floor of the elevator
-		} else if cost > 0 {
-			if status.Dir == common.UpDir || status.Dir == common.NoDir {
-				costs.CostsUp[i] = cost
-				costs.CostsDown[i] = penaltyTargetDir * cost
-				costs.CostsCab[i] = cabPenalty * cost
-			}
-		//Order is placed at the last floor the elevator was at
-		} else if cost == 0 {
-			cost = 1
-			if status.Dir == common.NoDir {
-				costs.CostsUp[i] = cost
-				costs.CostsDown[i] = cost
-				costs.CostsCab[i] = cost
-			}
+	//We only care about the distance if the elevator has no other orders
+	if orderCount == 0 {
+		for i := 0; i < len(orders.Cab); i++ {
+			cost := math.Abs(float64(i - status.Floor))
+			newCost.Cab[i] = cost + 0.5
+			newCost.HallDown[i] = cost
+			newCost.HallUp[i] = cost
 		}
-		//Worst case elevator moving up and away from target floor of the order
-		if (cost < 0 && status.Dir == common.UpDir) || (cost == 0 && status.Dir == common.UpDir) {
-			if cost < 0 {
-				cost = -cost
-			} else {
-				cost = 1
-			}
-			costs.CostsUp[i] = penaltyToTarget * penaltyTargetDir * cost
-			costs.CostsDown[i] = penaltyToTarget * cost
-			costs.CostsCab[i] = penaltyToTarget * cabPenalty * cost
+	} else {
+
+		costCounter := 0
+		var searchDirIncrement int           //Current direction, either 1 or -1
+		var currentQueue []*SchedulableOrder //Current queue (up/down) we are looking in. Must represent same direction as searchdir
+		var currCost []float64               //Current cost slice we are adding cost to. Must represent same direction as searchdir
+		startFloor := status.Floor           //Floor to start searching
+
+		//Select search parameters based on prefered order direction
+		if status.OrderDir == common.UpDir {
+			searchDirIncrement = 1
+			currentQueue = orders.HallUp
+			currCost = newCost.HallUp
+		} else {
+			searchDirIncrement = -1
+			currentQueue = orders.HallDown
+			currCost = newCost.HallDown
 		}
-		//Worst case elevator moving down and away from target floor of the order
-		if (cost > 0 && status.Dir == common.DownDir) || (cost == 0 && status.Dir == common.DownDir) {
-			if cost == 0 {
-				cost = 1
+
+		//If moving, start from next floor in search
+		if status.Moving {
+			startFloor += searchDirIncrement
+		}
+
+		//Current floor
+		currFloor := startFloor
+
+		//Add incrementing cost in prefered direction
+		for ; currFloor < len(currentQueue) && currFloor >= 0; currFloor += searchDirIncrement {
+			orderCost := float64(costCounter + orderCount)
+			currCost[currFloor] = orderCost
+			newCost.Cab[currFloor] = orderCost + cabPenalty
+			costCounter++
+		}
+
+		//Switch cost and order lists based on direction
+		if searchDirIncrement > 0 {
+			currentQueue = orders.HallDown
+			currCost = newCost.HallDown
+		} else {
+			currentQueue = orders.HallUp
+			currCost = newCost.HallUp
+		}
+		//Switch direction
+		searchDirIncrement *= -1
+
+		//Add incrementing cost in opposite direction
+		for currFloor += searchDirIncrement; currFloor < len(currentQueue) && currFloor >= 0; currFloor += searchDirIncrement {
+			orderCost := float64(costCounter + orderCount)
+			currCost[currFloor] = orderCost
+			//If no cab cost have been added already
+			if newCost.Cab[currFloor] <= cabPenalty/2 {
+				newCost.Cab[currFloor] = orderCost + cabPenalty
 			}
-			costs.CostsUp[i] = penaltyToTarget * cost
-			costs.CostsDown[i] = penaltyToTarget * penaltyTargetDir * cost
-			costs.CostsCab[i] = penaltyToTarget * cabPenalty * cost
+			costCounter++
+		}
+
+		//Switch back to first direction
+		if searchDirIncrement > 0 {
+			currentQueue = orders.HallDown
+			currCost = newCost.HallDown
+		} else {
+			currentQueue = orders.HallUp
+			currCost = newCost.HallUp
+		}
+		//Switch direction
+		searchDirIncrement *= -1
+		//Add incrementing cost for the prefered direction, but for the order that are located in the opposite direction
+		//Multiple direction switches will be neccessary to fulfill these orders
+		for currFloor += searchDirIncrement; currFloor != startFloor; currFloor += searchDirIncrement {
+			orderCost := float64(costCounter + orderCount)
+			currCost[currFloor] = orderCost
+			costCounter++
 		}
 	}
+
+	//Set the new costs
+	return newCost
 }
 
-*/
+func countOrdersWithID(orders *schedOrders, id int) int {
+	orderCount := 0
+
+	allOrders := make([]*SchedulableOrder, 0, len(orders.Cab)+len(orders.HallDown)+len(orders.HallUp))
+	allOrders = append(allOrders, orders.Cab...)
+	allOrders = append(allOrders, orders.HallDown...)
+	allOrders = append(allOrders, orders.HallUp...)
+	//Count valid, existing orders for this elevator
+	for _, order := range allOrders {
+		if order != nil && order.Worker == id {
+			orderCount++
+		}
+	}
+	return id
+}
