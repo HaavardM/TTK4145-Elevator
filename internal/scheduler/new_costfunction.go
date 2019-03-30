99415e067dda38a9b74eee48ef98f98cffc1ed01
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-03-30 14:10:42 +0100

    Fix cost function

--- internal/scheduler/new_costfunction.go
+++ internal/scheduler/new_costfunction.go
@@ -1,6 +1,8 @@
 package scheduler
 
 import (
+	"math"
+
 	"github.com/TTK4145-students-2019/project-thefuturezebras/internal/common"
 )
 
@@ -25,66 +27,77 @@ func updateElevatorCost(costs *common.OrderCosts, status common.ElevatorStatus, 
 		CostsUp:    make([]float64, len(costs.CostsUp)),
 		CostsDown:  make([]float64, len(costs.CostsDown)),
 	}
-	costCount := 0
-	var searchDir int
-	var currentQueue []*SchedulableOrder
-	var currCost []float64
-	startFloor := status.Floor
 
-	if status.Dir == common.UpDir {
-		searchDir = 1
-		currentQueue = orders.OrdersUp
-		currCost = newCost.CostsUp
-	} else {
-		searchDir = -1
-		currentQueue = orders.OrdersDown
-		currCost = newCost.CostsDown
-	}
-	if status.Moving {
-		startFloor += searchDir
-	}
-	currFloor := startFloor
-
-	for ; currFloor < len(currentQueue) && currFloor >= 0; currFloor += searchDir {
-		orderCost := float64(costCount + orderCount)
-		currCost[currFloor] = orderCost
-		newCost.CostsCab[currFloor] = orderCost + 0.5
-		costCount++
-	}
-
-	if searchDir > 0 {
-		currentQueue = orders.OrdersDown
-		currCost = newCost.CostsDown
-	} else {
-		currentQueue = orders.OrdersUp
-		currCost = newCost.CostsUp
-	}
-	//Switch direction
-	searchDir *= -1
-	//Search opposite direction
-	for currFloor += searchDir; currFloor < len(currentQueue) && currFloor >= 0; currFloor += searchDir {
-		orderCost := float64(costCount + orderCount)
-		currCost[currFloor] = orderCost
-		if newCost.CostsCab[currFloor] <= 0.25 {
-			newCost.CostsCab[currFloor] = orderCost + 0.5
+	if orderCount == 0 {
+		for i := 0; i < len(orders.OrdersCab); i++ {
+			cost := math.Abs(float64(i - status.Floor))
+			newCost.CostsCab[i] = cost + 0.5
+			newCost.CostsDown[i] = cost
+			newCost.CostsUp[i] = cost
 		}
-		costCount++
-	}
-
-	if searchDir > 0 {
-		currentQueue = orders.OrdersDown
-		currCost = newCost.CostsDown
 	} else {
-		currentQueue = orders.OrdersUp
-		currCost = newCost.CostsUp
-	}
-	//Switch direction
-	searchDir *= -1
-	//Search opposite direction
-	for currFloor += searchDir; currFloor != startFloor; currFloor += searchDir {
-		orderCost := float64(costCount + orderCount)
-		currCost[currFloor] = orderCost
-		costCount++
+
+		costCount := 0
+		var searchDir int
+		var currentQueue []*SchedulableOrder
+		var currCost []float64
+		startFloor := status.Floor
+
+		if status.Dir == common.UpDir {
+			searchDir = 1
+			currentQueue = orders.OrdersUp
+			currCost = newCost.CostsUp
+		} else {
+			searchDir = -1
+			currentQueue = orders.OrdersDown
+			currCost = newCost.CostsDown
+		}
+		if status.Moving {
+			startFloor += searchDir
+		}
+		currFloor := startFloor
+
+		for ; currFloor < len(currentQueue) && currFloor >= 0; currFloor += searchDir {
+			orderCost := float64(costCount + orderCount)
+			currCost[currFloor] = orderCost
+			newCost.CostsCab[currFloor] = orderCost + 0.5
+			costCount++
+		}
+
+		if searchDir > 0 {
+			currentQueue = orders.OrdersDown
+			currCost = newCost.CostsDown
+		} else {
+			currentQueue = orders.OrdersUp
+			currCost = newCost.CostsUp
+		}
+		//Switch direction
+		searchDir *= -1
+		//Search opposite direction
+		for currFloor += searchDir; currFloor < len(currentQueue) && currFloor >= 0; currFloor += searchDir {
+			orderCost := float64(costCount + orderCount)
+			currCost[currFloor] = orderCost
+			if newCost.CostsCab[currFloor] <= 0.25 {
+				newCost.CostsCab[currFloor] = orderCost + 0.5
+			}
+			costCount++
+		}
+
+		if searchDir > 0 {
+			currentQueue = orders.OrdersDown
+			currCost = newCost.CostsDown
+		} else {
+			currentQueue = orders.OrdersUp
+			currCost = newCost.CostsUp
+		}
+		//Switch direction
+		searchDir *= -1
+		//Search opposite direction
+		for currFloor += searchDir; currFloor != startFloor; currFloor += searchDir {
+			orderCost := float64(costCount + orderCount)
+			currCost[currFloor] = orderCost
+			costCount++
+		}
 	}
 	*costs = newCost
 }
