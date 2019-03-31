e09c1f4e54e446bc01911fbd7de66bdb508b1316
sondreo <sondrols@stud.ntnu.no>
2019-03-31 18:05:31 +0200

    fix

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -106,8 +106,8 @@ func Run(ctx context.Context, waitGroup *sync.WaitGroup, conf Config) {
 		},
 	}
 
-	orderTimeout := 20 * time.Second
-	orderTimeoutTicker := time.NewTicker(5*time.Second)
+	orderTimeout := 15 * time.Second
+	orderTimeoutTicker := time.NewTicker(time.Second)
 	//Channel used to avoid select blocking when neccessary
 	skipSelect := make(chan struct{}, 1)
 
