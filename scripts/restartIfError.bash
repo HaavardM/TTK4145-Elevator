6ffb41929ca06e1e2987faa1216746284c90b97e
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-04-01 09:45:08 +0200

    Add scripts for testing

--- scripts/restartIfError.bash
+++ scripts/restartIfError.bash
@@ -4,4 +4,5 @@ EXIT_CODE=1
 while [ $EXIT_CODE -gt 0 ]; do 
     $@
     EXIT_CODE=$?
+    sleep 1s
 done
---
+++ scripts/setupProject.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PATH=$GOPATH/src/github.com/TTK4145-students-2019
+
+set -e
+mkdir -p $PATH
+set +e
+
+cd $PATH
+
+git clone https://github.com/TTK4145-students-2019/project-thefuturezebras.git
+
+cd $PATH/project-thefuturezebras.git
+
+
