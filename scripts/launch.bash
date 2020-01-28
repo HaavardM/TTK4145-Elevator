e5357b48732f91eaeb45dd52907ebd0be37546f1
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2020-01-28 21:38:14 +0100

    Update tmux launch file

--- scripts/launch.bash
+++ scripts/launch.bash
@@ -3,8 +3,10 @@
 go build -o main ../main.go
 
 tmux new-session -s sim -n sim1 -d './SimElevatorServer --port=1234'
-tmux new-window -t sim:1 -n sim2 './SimElevatorServer --port=2345'
-tmux new-window -t sim:2 -n elev1 './restartIfError.bash ./main --elevator-port=1234 --id=1 --folder=/home/haavard/orders1'
-tmux new-window -t sim:3 -n elev2 './restartIfError.bash ./main --elevator-port=2345 --id=2 --folder=/home/haavard/orders2'
+tmux split-window -t sim:0 -d './SimElevatorServer --port=2345'
+tmux split-window -t sim:0 -d './restartIfError.bash ./main --elevator-port=1234 --id=1 --folder=orders1.json'
+tmux split-window -t sim:0 -d './restartIfError.bash ./main --elevator-port=2345 --id=2 --folder=orders2.json'
 
-tmux join-pane 
+tmux select-layout -t sim:0 tiled
+
+tmux attach -t sim
